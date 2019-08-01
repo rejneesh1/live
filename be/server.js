e753fc948f65336c307c8b0743451062bed64642
rejneesh <pillairejneesh@gmail.com>
2019-08-01 22:02:37 +0530

    rm gitignore

--- be/server.js
+++ be/server.js
@@ -37,7 +37,7 @@ mongoose.connect(dbConfig.url, {
 
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4300');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');  
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
